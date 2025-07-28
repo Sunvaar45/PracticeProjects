c1fafa4b9cb6facdf0432f19d8f8ca1c34f7ebdd
Sunvaar45 <is.kalay1@gmail.com>
2025-07-28 17:28:10 +0300

    Move files to javascript.info directory structure
    Renamed and reorganized JavaScript practice files into a new 'javascript.info' directory structure

---
+++ JavascriptPractice/javascript.info/4data_types/11date_and_time/file2.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// getFullYear() - 4 digit
+// getMonth() - from 0 to 11
+// getDate() - from 1 to 31
+// getHours(), getMinutes(), getSeconds(), getMilliseconds()
+// getDay() - from 0 (sunday) to 6 (saturday)
+// all methods above have getUtc... counterparts which are in UTC+0
+
+console.log(  )
