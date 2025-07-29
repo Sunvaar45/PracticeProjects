58579876398206012bad9a989ad0de84ae973539
Sunvaar45 <is.kalay1@gmail.com>
2025-07-29 15:13:31 +0300

    Add sumAll and showName functions demonstrating rest parameters

---
+++ JavascriptPractice/javascript.info/5advanced_working_with_functions/2rest_parameters_and_spread_syntax/file.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// rest parameters "..."
+function sumAll(...args) {
+    let result = args.reduce(function(sum, value) {
+        return sum + value;
+    }, 0);
+    return result;
+}
+console.log( sumAll(1, 2, 3) ); // 6
+console.log( sumAll(5, 10, 20, 50, 1, 7) ); // 93
+
+
+function showName(firstName, lastName, ...titles) {
+    console.log(`Name: ${firstName} ${lastName}`);
+    console.log(`Titles: ${titles}`);
+}
+showName("Donquixote", "Doflamingo", "Heavenly Demon", "Warlord of the Sea", "Fallen Celestial Dragon");4
+
+// ... puts args into an array
+// ...rest always has to be the last arg
