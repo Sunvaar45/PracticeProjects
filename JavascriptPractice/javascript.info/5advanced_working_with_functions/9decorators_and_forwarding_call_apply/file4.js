27c9b431ba34a2890200b085e4535fd836fec27e
Sunvaar45 <is.kalay1@gmail.com>
2025-08-05 15:03:17 +0300

    Implement caching decorator and hash function for multi-argument handling

--- JavascriptPractice/javascript.info/5advanced_working_with_functions/9decorators_and_forwarding_call_apply/file4.js
+++ JavascriptPractice/javascript.info/5advanced_working_with_functions/9decorators_and_forwarding_call_apply/file4.js
@@ -1,3 +1,39 @@
 "use strict";
 
-/* going multi argument */
+/* going multi argument and func.apply */
+let worker = {
+    slow(min, max) {
+        return min + max;
+    },
+};
+
+function cachingDecorator(func, hash) {
+    let cache = new Map();
+
+    return function() {
+        let key = hash(arguments);
+        if (cache.has(key)) {
+            console.log(`Called from cache`);
+            return cache.get(key);
+        }
+
+        console.log(`Called with function, args: ${key}`);
+        // let result = func.call(this, ...arguments);
+        let result = func.apply(this, arguments);
+
+        cache.set(key, result);
+        return result;
+    };
+}
+
+function hash(args) {
+    // return args[0] + "," + args[1];
+    // Method borrowing - better hash
+    return [].join.call(args);
+        // arguments.join() but arguments is not an array, it doesnt have this method
+        // we borrow join from empty array and put arguments as context
+}
+
+worker.slow = cachingDecorator(worker.slow, hash);
+console.log( worker.slow(3, 5) );
+console.log( worker.slow(3, 5) );
