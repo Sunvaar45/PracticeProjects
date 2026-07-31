440b91ee4181a3f3895143a183f7a712b87e213a
Sunvaar45 <is.kalay1@gmail.com>
2026-07-31 12:39:44 +0300

    add CreateBook method to BooksController for book creation functionality

--- C#Practice/BTK_APİ_education/bookDemo/Controllers/BooksController.cs
+++ C#Practice/BTK_APİ_education/bookDemo/Controllers/BooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using bookDemo.Data;
+using bookDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bookDemo.Controllers
@@ -33,5 +34,24 @@ namespace bookDemo.Controllers
 
             return Ok(book);
         }
+
+        [HttpPost]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book == null)
+                {
+                    return BadRequest("Book cannot be null.");
+                }
+
+                ApplicationContext.Books.Add(book);
+                return StatusCode(201, book); // 201 Created
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }   
     }
 }
