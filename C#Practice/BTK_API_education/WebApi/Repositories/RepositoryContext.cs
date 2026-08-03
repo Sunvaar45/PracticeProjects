4f9cbd458e4bafd9ed9808899a565f082ca617cb
Sunvaar45 <is.kalay1@gmail.com>
2026-08-03 12:05:12 +0300

    configure Entity Framework Core with SQL Server and update appsettings for database connection

--- C#Practice/BTK_API_education/WebApi/Repositories/RepositoryContext.cs
+++ C#Practice/BTK_API_education/WebApi/Repositories/RepositoryContext.cs
@@ -9,6 +9,10 @@ namespace WebApi.Repositories
 {
     public class RepositoryContext : DbContext
     {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+        }
+
         public DbSet<Book> Books { get; set; }
     }
 }
