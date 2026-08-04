3a8e5835a1bad23a8207d54a0bb8cb7f5ef9e80e
Sunvaar45 <is.kalay1@gmail.com>
2026-08-04 16:03:19 +0300

    feat: add RepositoryContextFactory for DbContext creation

---
+++ C#Practice/BTK_API_education/WebApi/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repositories.EFCore;
+
+namespace WebApi.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            // ConfigurationBuilder
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // DbContextOptionsBuilder
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    prj => prj.MigrationsAssembly("WebApi")
+                );
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
