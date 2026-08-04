d1719995fa5b33fb7c8816bc03996587f2b5ea62
Sunvaar45 <is.kalay1@gmail.com>
2026-08-04 13:46:37 +0300

    feat: implement ServiceManager class and IServiceManager interface for service management

---
+++ C#Practice/BTK_API_education/Services/ServiceManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IBookService> _bookService;
+        public ServiceManager(IRepositoryManager repositoryManager)
+        {
+            _bookService = new Lazy<IBookService>(() => new BookService(repositoryManager));
+        }
+            
+
+        public IBookService BookService => _bookService.Value;
+    }
+}
