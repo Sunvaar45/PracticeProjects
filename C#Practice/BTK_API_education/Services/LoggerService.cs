731b1cc0a5b011638777e1172dbb564e9f4a0f06
Sunvaar45 <is.kalay1@gmail.com>
2026-08-05 10:20:31 +0300

    feat: implement LoggerService class for logging functionality

---
+++ C#Practice/BTK_API_education/Services/LoggerService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Services.Contracts;
+
+namespace Services
+{
+    public class LoggerService : ILoggerService
+    {
+        public void LogDebug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
