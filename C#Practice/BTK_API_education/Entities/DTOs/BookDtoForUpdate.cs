84d0399ffe5a54abb6a5a7ba0565163c0501ccf9
Sunvaar45 <is.kalay1@gmail.com>
2026-08-05 17:09:59 +0300

    feat: add AutoMapper configuration and create BookDtoForUpdate class

---
+++ C#Practice/BTK_API_education/Entities/DTOs/BookDtoForUpdate.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public record BookDtoForUpdate
+    {
+        public int Id { get; init; }
+        public string Title { get; init; } = string.Empty;
+        public decimal Price { get; init; }
+    }
+}
