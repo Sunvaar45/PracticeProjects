a7f8255e4bd90067be68e17bf2b86c3da67bee1e
Sunvaar45 <is.kalay1@gmail.com>
2026-08-11 12:49:44 +0300

    feat: update GetBooksAsync method to accept BookParameters for pagination support

--- C#Practice/BTK_API_education/Presentation/Controllers/BooksController.cs
+++ C#Practice/BTK_API_education/Presentation/Controllers/BooksController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Entities.DTOs;
 using Entities.Exceptions;
 using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Presentation.ActionFilters;
@@ -25,9 +26,9 @@ namespace Presentation.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetBooksAsync()
+        public async Task<IActionResult> GetBooksAsync([FromQuery] BookParameters bookParameters)
         {
-            var books = await _manager.BookService.GetAllBooksAsync(false);
+            var books = await _manager.BookService.GetAllBooksAsync(bookParameters, false);
             return Ok(books);
         }
 
