101c742b980f89de6493d23b77daf3739e942465
Sunvaar45 <is.kalay1@gmail.com>
2026-08-11 12:29:35 +0300

    feat: update GetAllBooksAsync method to include BookParameters for pagination support

--- C#Practice/BTK_API_education/Repositories/Contracts/IBookRepository.cs
+++ C#Practice/BTK_API_education/Repositories/Contracts/IBookRepository.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Entities.Models;
+using Entities.RequestFeatures;
 
 namespace Repositories.Contracts
 {
     public interface IBookRepository : IRepositoryBase<Book>
     {
-        Task<IEnumerable<Book>> GetAllBooksAsync(bool trackChanges);
+        Task<IEnumerable<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges);
 
         Task<Book> GetBookByIdAsync(int id, bool trackChanges);
 
