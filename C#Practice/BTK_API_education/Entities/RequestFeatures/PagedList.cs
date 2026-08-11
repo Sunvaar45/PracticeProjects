05cd6ac5dad1dab331fa22857441cd393769629e
Sunvaar45 <is.kalay1@gmail.com>
2026-08-11 15:08:58 +0300

    feat: implement ToPagedList method for pagination functionality

--- C#Practice/BTK_API_education/Entities/RequestFeatures/PagedList.cs
+++ C#Practice/BTK_API_education/Entities/RequestFeatures/PagedList.cs
@@ -21,5 +21,18 @@ namespace Entities.RequestFeatures
             };
             AddRange(items);
         }
+
+        // 
+        public static PagedList<T> ToPagedList(IEnumerable<T> source,
+            int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
