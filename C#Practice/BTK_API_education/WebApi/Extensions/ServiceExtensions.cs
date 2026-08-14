c89552d03bfec84f1a57b7b3eb2c73079b7eb464
Sunvaar45 <is.kalay1@gmail.com>
2026-08-14 13:55:16 +0300

    [Hateoas] feat: add custom media types for HATEOAS support in API responses

--- C#Practice/BTK_API_education/WebApi/Extensions/ServiceExtensions.cs
+++ C#Practice/BTK_API_education/WebApi/Extensions/ServiceExtensions.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Entities.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Presentation.ActionFilters;
 using Repositories.Contracts;
@@ -57,6 +59,32 @@ namespace WebApi.Extensions
         public static void ConfigureDataShaper(this IServiceCollection services)
         {
             services.AddScoped<IDataShaper<BookDto>, DataShaper<BookDto>>();
+        }
+
+        public static void AddCustomMediaTypes(this IServiceCollection services)
+        {
+            services.Configure<MvcOptions>(config =>
+            {
+                var systemTextJsonOutputFormatter = config
+                    .OutputFormatters
+                    .OfType<SystemTextJsonOutputFormatter>()?.FirstOrDefault();
+
+                if (systemTextJsonOutputFormatter != null)
+                {
+                    systemTextJsonOutputFormatter.SupportedMediaTypes
+                        .Add("application/vnd.myapp.hateoas+json");
+                }
+
+                var xmlOutputFormatter = config
+                    .OutputFormatters
+                    .OfType<XmlDataContractSerializerOutputFormatter>()?.FirstOrDefault();
+
+                if (xmlOutputFormatter != null)
+                {
+                    xmlOutputFormatter.SupportedMediaTypes
+                        .Add("application/vnd.myapp.hateoas+xml");
+                }
+            });
         }
     }
 }
--- C#Practice/BTK_API_education/WebApi/Program.cs
+++ C#Practice/BTK_API_education/WebApi/Program.cs
@@ -45,6 +45,7 @@ builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ConfigureActionFilters();
 builder.Services.ConfigureCors();
 builder.Services.ConfigureDataShaper();
+builder.Services.AddCustomMediaTypes();
 
 var app = builder.Build();
 
