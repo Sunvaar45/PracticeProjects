f4a98368f4a35f59ef469b7e04f5d3fd6e33b195
Sunvaar45 <is.kalay1@gmail.com>
2026-08-18 16:30:39 +0300

    [Versioning] feat: update API versioning for BooksController and BooksV2Controller, marking BooksV2Controller as deprecated

--- C#Practice/BTK_API_education/Presentation/Controllers/BooksController.cs
+++ C#Practice/BTK_API_education/Presentation/Controllers/BooksController.cs
@@ -15,7 +15,6 @@ using Services.Contracts;
 
 namespace Presentation.Controllers
 {
-    [ApiVersion("1.0")]
     [ServiceFilter(typeof(LogFilterAttribute))] 
     [ApiController]
     [Route("api/books")]
