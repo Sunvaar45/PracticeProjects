79ba201b39e9e6b3d24a7945a90d07caf362b61c
Sunvaar45 <is.kalay1@gmail.com>
2026-08-18 12:47:54 +0300

    [Versioning] feat: add API versioning configuration and integrate it into the service collection

--- C#Practice/BTK_API_education/WebApi/Extensions/ServiceExtensions.cs
+++ C#Practice/BTK_API_education/WebApi/Extensions/ServiceExtensions.cs
@@ -101,5 +101,15 @@ namespace WebApi.Extensions
         {
             services.AddScoped<IBookLinks, BookLinks>();
         }
+
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
     }
 }
--- C#Practice/BTK_API_education/WebApi/Program.cs
+++ C#Practice/BTK_API_education/WebApi/Program.cs
@@ -49,6 +49,7 @@ builder.Services.ConfigureCors();
 builder.Services.ConfigureDataShaper();
 builder.Services.AddCustomMediaTypes();
 builder.Services.ConfigureBookLinks();
+builder.Services.ConfigureVersioning();
 
 var app = builder.Build();
 
