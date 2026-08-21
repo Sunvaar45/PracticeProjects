293723c32b04ff2b2fdd8eef6926eb5cf6ecd7a1
Sunvaar45 <is.kalay1@gmail.com>
2026-08-21 12:07:18 +0300

    [Security] feat: add IAuthenticationService to IServiceManager and implement lazy loading in ServiceManager

--- C#Practice/BTK_API_education/Services/ServiceManager.cs
+++ C#Practice/BTK_API_education/Services/ServiceManager.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Entities.DTOs;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Repositories.Contracts;
 using Services.Contracts;
 
@@ -12,14 +15,24 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
+        private readonly Lazy<IAuthenticationService> _authenticationService;
+
         public ServiceManager(IRepositoryManager repositoryManager, 
             ILoggerService logger, 
+            UserManager<User> userManager,
+            IConfiguration configuration,
             IMapper mapper,
             IBookLinks bookLinks)
         {
-            _bookService = new Lazy<IBookService>(() => new BookService(repositoryManager, logger, mapper, bookLinks));
+            _bookService = new Lazy<IBookService>(() => 
+            new BookService(repositoryManager, logger, mapper, bookLinks));
+            
+            _authenticationService = new Lazy<IAuthenticationService>(() => 
+            new AuthenticationService(logger, mapper, userManager, configuration)); 
         }
 
         public IBookService BookService => _bookService.Value;
+
+        public IAuthenticationService AuthenticationService => throw new NotImplementedException();
     }
 }
