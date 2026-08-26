458622a5a6a9400fca36c8d2d247c2ff7446e56c
Sunvaar45 <is.kalay1@gmail.com>
2026-08-26 14:34:13 +0300

    [Bonus] feat: add Categories table with seeding data in the database

---
+++ C#Practice/BTK_API_education/Repositories/EFCore/Config/CategoryConfig.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repositories.EFCore.Config
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.CategoryId);
+            builder.Property(c => c.CategoryName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasData(
+                new Category { CategoryId = 1, CategoryName = "Fiction" },
+                new Category { CategoryId = 2, CategoryName = "Non-Fiction" },
+                new Category { CategoryId = 3, CategoryName = "Science Fiction" },
+                new Category { CategoryId = 4, CategoryName = "Biography" }
+            );
+        }
+    }
+}
---
+++ C#Practice/BTK_API_education/Repositories/Migrations/20260826113156_AddAndSeedCategoriesTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAndSeedCategoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "Fiction" },
+                    { 2, "Non-Fiction" },
+                    { 3, "Science Fiction" },
+                    { 4, "Biography" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
