b9d90f3a6e3d45821b777aff3826531faba3c51d
Sunvaar45 <is.kalay1@gmail.com>
2026-08-26 14:27:38 +0300

    [Bonus] feat: add lazy loading for ICategoryRepository in RepositoryManager

--- C#Practice/BTK_API_education/Repositories/Contracts/IRepositoryManager.cs
+++ C#Practice/BTK_API_education/Repositories/Contracts/IRepositoryManager.cs
@@ -7,6 +7,7 @@ namespace Repositories.Contracts
 {
     public interface IRepositoryManager
     {
+        ICategoryRepository Category { get; }
         IBookRepository Book { get; }
 
         Task SaveAsync();
