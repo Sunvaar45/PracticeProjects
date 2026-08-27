0c8fc2235f0a460af1fec00eb1bca7d39b88ac21
Sunvaar45 <is.kalay1@gmail.com>
2026-08-27 15:11:42 +0300

    [Bonus] feat: add CategoryId property to BookDto

--- C#Practice/BTK_API_education/Entities/DTOs/BookDto.cs
+++ C#Practice/BTK_API_education/Entities/DTOs/BookDto.cs
@@ -10,5 +10,7 @@ namespace Entities.DTOs
         public int Id { get; init; }
         public string Title { get; init; } = string.Empty;
         public decimal Price { get; init; }
+
+        public int CategoryId { get; set; }
     }
 }
