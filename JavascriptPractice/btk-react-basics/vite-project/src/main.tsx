a05e102c8ab143d797c3be3e392620cbf74c398f
Sunvaar45 <is.kalay1@gmail.com>
2026-09-02 17:03:14 +0300

    [React Components] feat: integrate Bootstrap for styling and enhance Header component with navigation

--- JavascriptPractice/btk-react-basics/vite-project/src/main.tsx
+++ JavascriptPractice/btk-react-basics/vite-project/src/main.tsx
@@ -2,28 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 function App() {
   return (
     <>
       <Header />
-      <ProductList />
-      <Footer />
+      <div className="container">
+        <ProductList />
+        <Footer />
+      </div>
     </>
   );
 }
 
 function Header() {
-  const style = {
-    color: 'blue',
-    textAlign: 'center',
-    fontSize: '30px',
-    textTransform: 'uppercase',
-  }
-
   return (
-      <h1 style={ style }>Header</h1>
+      <header>
+        <nav className="navbar navbar-expand bg-dark border-bottom border-body" 
+          data-bs-theme="dark">
+            <div className="container">
+              <a href="#" className="navbar-brand">Store App</a>
+            </div>
+        </nav>
+      </header>
   );
 }
 
