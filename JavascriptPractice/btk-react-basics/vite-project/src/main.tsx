78b126bec312b37f4925c7e7311bd457702a3b2f
Sunvaar45 <is.kalay1@gmail.com>
2026-09-02 15:11:26 +0300

    [React Components] feat: add Footer component to display store hours

--- JavascriptPractice/btk-react-basics/vite-project/src/main.tsx
+++ JavascriptPractice/btk-react-basics/vite-project/src/main.tsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <Header />
       <ProductList />
+      <Footer />
     </>
   );
 }
@@ -62,6 +63,26 @@ function ProductItem({ productObject }) {
       <p>{ productObject.description }</p>
       <span>{ productObject.price }</span>
     </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openingHour = 10;
+  const closingHour = 22;
+  const isOpen = hour >= openingHour && hour <= closingHour;
+  console.log(isOpen);
+
+  return (
+    <footer>
+      {
+        isOpen ? (
+          <p>We are open until { closingHour }:00.</p>
+        ) : (
+          <p>We are closed. Our working hours are from { openingHour }:00 to { closingHour }:00.</p>
+        )
+      }
+    </footer>
   );
 }
 
