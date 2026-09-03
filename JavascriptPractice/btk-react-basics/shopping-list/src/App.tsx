9b0391fcb669f40480b789c2f5c6cb39e9a9f0f8
Sunvaar45 <is.kalay1@gmail.com>
2026-09-03 13:49:20 +0300

    [React State & Events] feat: prepare shopping list logic with static components and styling

--- JavascriptPractice/btk-react-basics/shopping-list/src/App.tsx
+++ JavascriptPractice/btk-react-basics/shopping-list/src/App.tsx
@@ -1,10 +1,62 @@
 function App() {
 
   return (
-    <div>
-      <h1>Shopping List</h1>
+    <div className="App">
+      <Header />
+      <Form />
+      <List />
+      <Summary />
     </div>
   )
+}
+
+function Header() {
+  return (
+    <h1>Shopping List</h1>
+  )
+}
+
+function Form() {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Add an item" />
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
+function List() {
+  return (
+    <div className="list">
+      <ul>
+        <Item />
+        <Item />
+        <Item />
+      </ul>
+    </div>
+  )
+}
+
+function Item() {
+  return (
+    <li>
+      <span>Fruit</span>
+      <button>X</button>
+    </li>
+  )
+}
+
+function Summary() {
+  return (
+    <footer className="summary">
+      <p>Total items: 3</p>
+    </footer>
+  );
 }
 
 export default App
