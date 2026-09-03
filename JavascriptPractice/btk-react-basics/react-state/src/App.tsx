96689232cafab1e20c413296c2c790c739ae5fc8
Sunvaar45 <is.kalay1@gmail.com>
2026-09-03 11:43:03 +0300

    [React State & Events] feat: display sculpture details including name, artist, and description in App component

--- JavascriptPractice/btk-react-basics/react-state/src/App.tsx
+++ JavascriptPractice/btk-react-basics/react-state/src/App.tsx
@@ -19,7 +19,16 @@ function App() {
       <button onClick={ handleNextClick }>Next</button>
       <button onClick={ handlePreviousClick }>Previous</button>
 
-
+      <h2>
+        <i> {sculpture.name} </i> by {sculpture.artist}
+      </h2>
+      <h3>
+        (
+          { index + 1 } of { sculptureList.length }
+        )
+      </h3>
+      <img src={ sculpture.url } alt={ sculpture.alt }></img>
+      <p>{ sculpture.description }</p>
     </>
   )
 }
