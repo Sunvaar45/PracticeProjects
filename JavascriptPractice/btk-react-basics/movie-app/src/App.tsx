952142866cfc68689f36846d64f04fda4c9171b9
Sunvaar45 <is.kalay1@gmail.com>
2026-09-04 17:47:17 +0300

    [Component Composition] feat: implement movie list display with state management and bootstrap styling in App component

--- JavascriptPractice/btk-react-basics/movie-app/src/App.tsx
+++ JavascriptPractice/btk-react-basics/movie-app/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const movie_list = [
   {
     Id: "769",
@@ -29,7 +31,26 @@ const movie_list = [
   },
 ];
 
+const selected_movie_list = [
+  {
+    Id: "769",
+    Title: "GoodFellas",
+    Year: "1990",
+    Poster:
+      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
+  },
+  {
+    Id: "120",
+    Title: "The Lord of the Rings",
+    Year: "2001",
+    Poster:
+      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
+  },
+];
+
 function App() {
+  const [movies, setMovies] = useState(movie_list);
+
   return (
     <>
       <nav className="bg-primary text-white p-2">
@@ -51,9 +72,33 @@ function App() {
       </nav>
 
       <main className="container">
-        <div className="row mt2">
-          <div className="col md-9">Movie List</div>
-          <div className="col md-3">Selected Movie List</div>
+        <div className="row mt-2">
+          <div className="col-md-9">
+            <div className="movie-list">
+              <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4">
+                {movies.map((movie) => (
+                  <div className="col mb-2" key={movie.Id}>
+                    <div className="card">
+                      <img
+                        className="card-img-top"
+                        src={movie.Poster}
+                        alt={movie.Title}
+                      ></img>
+                      <div className="card-body">
+                        <h6 className="card-title">{movie.Title}</h6>
+                        <div>
+                          <span className="badge bg-secondary">
+                            {movie.Year}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">Selected Movie List</div>
         </div>
       </main>
     </>
