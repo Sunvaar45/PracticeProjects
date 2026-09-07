98c3fecb2ab7f37ce44022b8c5d4030e90bcb3f5
Sunvaar45 <is.kalay1@gmail.com>
2026-09-07 14:41:57 +0300

    [Component Compositions] feat: refactor Navbar component by extracting NavLogo, NavSearch, and NavSearchResults into separate functions for improved readability

--- JavascriptPractice/btk-react-basics/movie-app/src/components/Navbar.tsx
+++ JavascriptPractice/btk-react-basics/movie-app/src/components/Navbar.tsx
@@ -3,22 +3,40 @@ export function Navbar() {
     <nav className="bg-primary text-white p-2">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-4">
-            <i className="bi bi-film me-2"></i>
-            Movie App
-          </div>
-          <div className="col-4">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search movies..."
-            ></input>
-          </div>
-          <div className="col-4 text-end">
-            <strong>5</strong> results found.
-          </div>
+          <NavLogo />
+          <NavSearch />
+          <NavSearchResults />
         </div>
       </div>
     </nav>
+  );
+}
+
+function NavSearchResults() {
+  return (
+    <div className="col-4 text-end">
+      <strong>5</strong> results found.
+    </div>
+  );
+}
+
+function NavSearch() {
+  return (
+    <div className="col-4">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search movies..."
+      ></input>
+    </div>
+  );
+}
+
+function NavLogo() {
+  return (
+    <div className="col-4">
+      <i className="bi bi-film me-2"></i>
+      Movie App
+    </div>
   );
 }
