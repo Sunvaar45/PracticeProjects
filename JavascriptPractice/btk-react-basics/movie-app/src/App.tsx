5b093d9a12a7775695adf13e793e4fa4b8ee7bc5
Sunvaar45 <is.kalay1@gmail.com>
2026-09-08 12:26:19 +0300

    [Component Compositions] feat: refactor App component to manage selected movies and integrate SelectedMovieList components

--- JavascriptPractice/btk-react-basics/movie-app/src/App.tsx
+++ JavascriptPractice/btk-react-basics/movie-app/src/App.tsx
@@ -5,15 +5,18 @@ import {
   NavSearchResults,
 } from "./components/Navbar";
 import { Main } from "./components/Main";
-import { movie_list } from "./data";
+import { movie_list, selected_movie_list } from "./data";
 import { useState } from "react";
 // import { MovieListContainer } from "./components/Movies/MovieListContainer";
 import { MovieList } from "./components/Movies/MovieList";
-import { SelectedMovieListContainer } from "./components/SelectedMovies/SelectedMovieListContainer";
+// import { SelectedMovieListContainer } from "./components/SelectedMovies/SelectedMovieListContainer";
 import { ListContainer } from "./components/Shared/ListContainer";
+import { SelectedMovieList } from "./components/SelectedMovies/SelectedMovieList";
+import { SelectedMovieListSummary } from "./components/SelectedMovies/SelectedMovieListSummary";
 
 function App() {
   const [movies, setMovies] = useState(movie_list);
+  const [selectedMovies, setSelectedMovies] = useState(selected_movie_list);
 
   return (
     <>
@@ -34,7 +37,12 @@ function App() {
 
           {/* Selected Movie List */}
           <div className="col-md-3">
-            <SelectedMovieListContainer />
+            <ListContainer>
+              <>
+                <SelectedMovieListSummary selectedMovies={selectedMovies} />
+                <SelectedMovieList selectedMovies={selectedMovies} />
+              </>
+            </ListContainer>
           </div>
         </div>
       </Main>
