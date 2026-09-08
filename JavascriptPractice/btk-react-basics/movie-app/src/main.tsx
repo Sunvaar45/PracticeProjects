a68e863d008f4f45d3a22187e3962e7cf014dbb1
Sunvaar45 <is.kalay1@gmail.com>
2026-09-08 15:07:31 +0300

    [Component Composition] feat: replace StarRating component with App component in main render

--- JavascriptPractice/btk-react-basics/movie-app/src/main.tsx
+++ JavascriptPractice/btk-react-basics/movie-app/src/main.tsx
@@ -4,12 +4,9 @@ import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
-import StarRating from "./components/Shared/StarRating.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <App />
   </StrictMode>,
 );
