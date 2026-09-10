35d39849d470e5a23a42e45062dbf91221b2058a
Sunvaar45 <is.kalay1@gmail.com>
2026-09-10 15:15:36 +0300

    [UseEffect & APIs] feat: add loading state to MovieDetails component and display loading indicator while fetching movie details

--- JavascriptPractice/btk-react-basics/movie-app/src/components/Movies/MovieDetails.tsx
+++ JavascriptPractice/btk-react-basics/movie-app/src/components/Movies/MovieDetails.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { IMovieDetails } from "../../types";
+import { Loading } from "../Shared/Loading";
 
 interface MovieDetailsProps {
   selectedMovieId: number;
@@ -13,10 +14,13 @@ export function MovieDetails({
   API_KEY,
 }: MovieDetailsProps) {
   const [movieDetails, setMovieDetails] = useState<IMovieDetails | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
       async function getMovieDetails() {
+        setIsLoading(true);
+
         try {
           const response = await fetch(
             `https://api.themoviedb.org/3/movie/${selectedMovieId}?api_key=${API_KEY}`,
@@ -34,6 +38,8 @@ export function MovieDetails({
           setMovieDetails(data);
         } catch (error) {
           console.error(error);
+        } finally {
+          setIsLoading(false);
         }
       }
 
@@ -42,11 +48,16 @@ export function MovieDetails({
     [selectedMovieId, API_KEY],
   );
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div className="border p-2 mb-3">
       <div className="row">
         {movieDetails && (
           <>
+            {/* movie poster */}
             <div className="col-4">
               <img
                 className="img-fluid rounded"
@@ -60,6 +71,7 @@ export function MovieDetails({
               ></img>
             </div>
 
+            {/* movie details */}
             <div className="col-8">
               <h6>{movieDetails.title}</h6>
               <p>
@@ -73,6 +85,7 @@ export function MovieDetails({
               </p>
             </div>
 
+            {/* overview and genre badges */}
             <div className="col-12 border-top p-3 mt-3">
               <p>{movieDetails.overview}</p>
               <p>
